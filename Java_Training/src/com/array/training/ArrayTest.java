782c6bc6311f7cf489167e7cb54aeda4383fe0fc
nsmal <nsmal@LAPTOP-F79B5TDV>
2023-04-19 16:26:27 -0700

    committing changes

---
+++ Java_Training/src/com/array/training/ArrayTest.java
@@ -0,0 +1,14 @@
+package com.array.training;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+						
+		int arr[] 	= {1,2,3};
+		
+		System.out.println(arr);
+
+	}
+
+}
